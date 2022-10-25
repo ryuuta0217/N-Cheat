1c6a26d0f2477c01faaacfcfc4a4dd996675b1b9
Ryuta Iwakura <ryuuta.iwakura@gmail.com>
2022-10-25 18:50:31 +0900

    Popup#initialize, #setDefault moved to config.ts

--- src/popup.ts
+++ src/popup.ts
@@ -1,5 +1,5 @@
-import { log, Level } from "./logger";
-import { Config } from "./config";
+import { Level, log } from "./logger";
+import { Config, initialize, setDefault } from "./config";
 
 window.onload = () => {
     initialize().then(() => {
@@ -14,47 +14,12 @@ window.onload = () => {
         });
 
         (document.getElementById("reset") as HTMLButtonElement).onclick = () => {
-            setDefault(true).then(() => {
+            setDefault().then(() => {
+                load();
                 log("Popup", Level.INFO, "Configuration reset.")
             });
         };
     });
-}
-
-async function initialize() {
-    if ((await chrome.storage.sync.getBytesInUse()) == 0) {
-        log("Popup", Level.INFO, "Configuration not found, writing default configuration.");
-        await setDefault(false);
-    }
-}
-
-async function setDefault(reload: boolean) {
-    await chrome.storage.sync.clear();
-    const defaultData: Config = {
-        useDesktopNotification: true,
-        useDiscordNotification: false,
-        discordWebhookUrl: "",
-        discordMention: "",
-        startPlaybackWhenOpenPage: false,
-        useAutoNext: true,
-        useAutoNextContainsSupplements: false,
-        useAutoNextNotGoodOnly: true,
-        useAutoPauseUnblock: true,
-        useSeekUnblock: true,
-        notifyVideoStateChange: false,
-        discordPlaybackStartedMessage: "%mention% 教材動画 `%title%` の再生を開始しました",
-        discordPlaybackEndedMessage: "%mention% 教材動画 `%title%` の再生が終了しました",
-        discordTakeTestMessage: "%mention% テスト `%title%` を受けてください",
-        desktopPlaybackStarted: "教材動画の再生を開始しました",
-        desktopPlaybackEnded: "教材動画の再生が終了しました",
-        desktopTakeTest: "テストを受けてください",
-        unknownVideo: "不明な動画",
-        unknownTest: "不明なテスト"
-    };
-
-    await chrome.storage.sync.set(defaultData);
-    log("Popup", Level.INFO, "Configuration initialized.");
-    if (reload) load();
 }
 
 function load() {
